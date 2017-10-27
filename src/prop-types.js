f73e646823f9d6055d12bf87071183b0fdfc8005
Jukka Rautanen <jukka@rautanen.info>
2017-10-27 12:34:18 +0300

    Init

---
+++ src/prop-types.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+
+/**
+ * Default values
+ */
+export default {
+  fixtures: PropTypes.array,
+  maxFixtures: PropTypes.number,
+  initialValue: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  inputClassName: PropTypes.string,
+  suggestsClassName: PropTypes.string,
+  suggestsHiddenClassName: PropTypes.string,
+  suggestItemClassName: PropTypes.string,
+  suggestItemActiveClassName: PropTypes.string,
+  location: PropTypes.object,
+  radius: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  bounds: PropTypes.object,
+  country: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array
+  ]),
+  types: PropTypes.array,
+  queryDelay: PropTypes.number,
+  googleMaps: PropTypes.object,
+  highlightMatch: PropTypes.bool,
+  onSuggestSelect: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  skipSuggest: PropTypes.func,
+  getSuggestLabel: PropTypes.func,
+  renderSuggestItem: PropTypes.func,
+  autoActivateFirstSuggest: PropTypes.bool,
+  style: PropTypes.shape({
+    input: PropTypes.object,
+    suggests: PropTypes.object,
+    suggestItem: PropTypes.object
+  }),
+  ignoreTab: PropTypes.bool,
+  label: PropTypes.string,
+  autoComplete: PropTypes.string,
+  minLength: PropTypes.number
+};
